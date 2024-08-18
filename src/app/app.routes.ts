d1d40bf056f93a8a1c8bb6f7b8ec066fcf97c376
carlacarrasco <carlacarr37@gmail.com>
2024-08-18 10:43:05 -0500

    Setup routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { BookmarkedComponent } from './pages/bookmarked/bookmarked.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MoviesComponent } from './pages/movies/movies.component';
+import { TvSeriesComponent } from './pages/tv-series/tv-series.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'bookmarked', component: BookmarkedComponent },
+    { path: 'movies', component: MoviesComponent },
+    { path: 'tv-series', component: TvSeriesComponent },
+];
